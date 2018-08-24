f4f3a2df45fd6532b22cb064742f0be4b2bb1d5c
nordpool <beelzenord@gmail.com>
2018-08-24 22:54:34 +0200

    in branch

--- splan/src/splan/Splan.java
+++ splan/src/splan/Splan.java
@@ -15,7 +15,7 @@ public class Splan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         System.out.println("branch");
     }
     
 }
